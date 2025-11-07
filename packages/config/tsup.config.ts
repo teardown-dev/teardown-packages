37afc4ff078a0b2b81e6228ee917ad2446daebb8
Chris Mitchell <chris@delacour.co.nz>
2025-11-06 21:15:09 -0800

    Add new config package for shared configs

---
+++ packages/config/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: ['./src/index.ts'],
+    outDir: './dist',
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+})
