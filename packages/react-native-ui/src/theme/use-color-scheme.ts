e2b474affd6d18bad41716d7339a809b0452a8eb
Chris Mitchell <chris@delacour.co.nz>
2025-11-06 21:15:09 -0800

    Fix building and bundling

--- packages/react-native-ui/src/theme/use-color-scheme.ts
+++ packages/react-native-ui/src/theme/use-color-scheme.ts
@@ -1,21 +1,49 @@
-import {useColorScheme as useNativewindColorScheme} from 'nativewind';
+import { useColorScheme as useNativewindColorScheme } from "nativewind";
 
 export function useColorScheme() {
-  const {colorScheme, setColorScheme: setNativeWindColorScheme} =
-    useNativewindColorScheme();
+	const { colorScheme, setColorScheme: setNativeWindColorScheme } =
+		useNativewindColorScheme();
 
-  const setColorScheme = async (colorScheme: 'light' | 'dark') => {
-    setNativeWindColorScheme(colorScheme);
-  };
+	const setColorScheme = async (colorScheme: "light" | "dark") => {
+		setNativeWindColorScheme(colorScheme);
+	};
 
-  const toggleColorScheme = () => {
-    return setColorScheme(colorScheme === 'light' ? 'dark' : 'light');
-  };
+	const toggleColorScheme = () => {
+		return setColorScheme(colorScheme === "light" ? "dark" : "light");
+	};
 
-  return {
-    colorScheme: colorScheme ?? 'dark',
-    isDarkColorScheme: colorScheme === 'dark',
-    setColorScheme,
-    toggleColorScheme,
-  };
+	return {
+		colorScheme: colorScheme ?? "dark",
+		isDarkColorScheme: colorScheme === "dark",
+		setColorScheme,
+		toggleColorScheme,
+		tokens: {
+			color: {
+				action: {
+					primary: {
+						foreground: {
+							dark: "#000",
+						},
+					},
+					secondary: {
+						foreground: {
+							dark: "#000",
+						},
+					},
+				},
+				foreground: {
+					default: {
+						dark: "#000",
+					},
+				},
+				interactive: {
+					default: {
+						foreground: {
+							dark: "#000",
+						},
+					},
+				},
+			},
+		},
+	};
 }
